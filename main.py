d5cdd7ace71865efc834551c8dc0f68d84ee77ad
dongmhuong12-netizen <dongmhuong12@gmail.com>
2026-02-24 23:24:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import discord
+from discord import app_commands
 import os
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True  # để có thể tag và xử lý member
 
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
+    await tree.sync()
     print(f'Logged in as {client.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content == "!ping":
-        await message.channel.send("Pong!")
+# Slash command /ping
+@tree.command(name="ping", description="Kiểm tra bot còn sống không")
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message("Pong!")
 
 client.run(os.getenv("TOKEN"))
