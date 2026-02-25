fc1f9992ec3134bf7721814175a4669476a8ca4e
dongmhuong12-netizen <dongmhuong12@gmail.com>
2026-02-25 20:01:35 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"🔥 Logged in as {bot.user}")
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+
+bot.run(TOKEN)
