607247639261aa3b0daea1bf93eeb94913966a43
dongmhuong12-netizen <dongmhuong12@gmail.com>
2026-02-25 21:17:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,12 @@
-import discord
-from discord import app_commands
-from discord.ext import commands
 import os
+from core.bot import create_bot
 
 TOKEN = os.getenv("TOKEN")
 
-intents = discord.Intents.default()
-intents.members = True
+if not TOKEN:
+    raise ValueError("❌ TOKEN not found in environment variables.")
 
+bot = create_bot()
 
-class PBot(commands.Bot):
-    def __init__(self):
-        super().__init__(
-            command_prefix="!",
-            intents=intents
-        )
-
-    async def setup_hook(self):
-        await self.tree.sync()
-        print("✅ Slash commands synced.")
-
-
-bot = PBot()
-
-
-@bot.event
-async def on_ready():
-    print(f"🔥 Logged in as {bot.user}")
-
-
-# ===== P SYSTEM GROUP =====
-p = app_commands.Group(
-    name="p",
-    description="Main P system"
-)
-
-bot.tree.add_command(p)
-
-
-@p.command(name="ping", description="Check bot status")
-async def ping(interaction: discord.Interaction):
-    await interaction.response.send_message("haii, pei is here!!´꒳`")
-
-
-bot.run(TOKEN)
+if __name__ == "__main__":
+    bot.run(TOKEN)
