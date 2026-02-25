16ce8056fe6744b712a2e365fa05e386018f9df5
dongmhuong12-netizen <dongmhuong12@gmail.com>
2026-02-26 04:51:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import discord
 from discord.ext import commands
 import asyncio
 
-
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
@@ -10,7 +9,6 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def main():
     async with bot:
         await bot.load_extension("core.root")
-        await bot.load_extension("commands.embed.embed_group")
         await bot.load_extension("commands.embed.create")
         await bot.start("YOUR_TOKEN")
 
