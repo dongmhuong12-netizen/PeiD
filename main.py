d005ac659d3430de6595bf4ebc3446eaec697045
dongmhuong12-netizen <dongmhuong12@gmail.com>
2026-02-26 04:54:01 +0700

    Use environment variable for bot token

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import asyncio
+import os
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -10,7 +11,7 @@ async def main():
     async with bot:
         await bot.load_extension("core.root")
         await bot.load_extension("commands.embed.create")
-        await bot.start("YOUR_TOKEN")
+        await bot.start(os.getenv("TOKEN"))
 
 
 asyncio.run(main())
