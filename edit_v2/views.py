91406789babd0f323ba40458fa4da94b958b39a0
dongmhuong12-netizen <dongmhuong12@gmail.com>
2026-02-25 18:26:16 +0700

    Update views.py

--- edit_v2/views.py
+++ edit_v2/views.py
@@ -1,1 +1,19 @@
+import discord
 
+class ConfirmView(discord.ui.View):
+
+    def __init__(self, timeout=30):
+        super().__init__(timeout=timeout)
+        self.value = None
+
+    @discord.ui.button(label="Confirm", style=discord.ButtonStyle.green)
+    async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.value = True
+        self.stop()
+        await interaction.response.defer()
+
+    @discord.ui.button(label="Cancel", style=discord.ButtonStyle.red)
+    async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.value = False
+        self.stop()
+        await interaction.response.defer()
