1ca1450ab9930c8d9ffee822679bbb611c3aca9d
dongmhuong12-netizen <dongmhuong12@gmail.com>
2026-02-26 04:56:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+    print(f"Logged in as {bot.user}")
+
+
 async def main():
     async with bot:
         await bot.load_extension("core.root")
