dd018404b286257dfd51fdfbf3d1f82d3a1bf699
dongmhuong12-netizen <dongmhuong12@gmail.com>
2026-02-27 02:24:32 +0700

    Update root.py

--- core/root.py
+++ core/root.py
@@ -3,7 +3,7 @@ from discord import app_commands
 from discord.ext import commands
 
 from storage.embed_storage import EmbedStorage
-from systems.embed_ui import EmbedUIView
+from core.embed_ui import EmbedUIView
 
 
 class Root(commands.Cog):
