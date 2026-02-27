3fe7596f49f1161210e7efea105f88d626de396b
dongmhuong12-netizen <dongmhuong12@gmail.com>
2026-02-27 08:05:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ intents.message_content = True
 
 bot = commands.Bot(
     command_prefix="!",
-    intents=intents,
-    application_id=1475879071857508393
+    intents=intents
 )
 
 
@@ -25,19 +24,18 @@ async def on_ready():
     print(f"Logged in as {bot.user} (ID: {bot.user.id})")
     print("------")
 
+    # Sync sau khi bot đã ready
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} slash commands globally.")
+    except Exception as e:
+        print(f"Sync error: {e}")
+
 
 async def main():
     async with bot:
         # Load extension trước khi start
         await bot.load_extension("core.root")
-
-        # Sync một lần duy nhất ở đây
-        try:
-            synced = await bot.tree.sync()
-            print(f"Synced {len(synced)} slash commands globally.")
-        except Exception as e:
-            print(f"Sync error: {e}")
-
         await bot.start(TOKEN)
 
 
