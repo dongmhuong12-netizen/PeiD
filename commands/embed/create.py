9be2600930f13858fb125197cd48c5800b93c456
dongmhuong12-netizen <dongmhuong12@gmail.com>
2026-03-07 01:50:23 +0700

    Update create.py

--- commands/embed/create.py
+++ commands/embed/create.py
@@ -1,36 +1,48 @@
-def __init__(self, bot):
-    self.bot = bot
+import discord
+from discord.ext import commands
+from core.root import Root
+from core.embed_ui import EmbedUIView
 
-    root: Root = bot.get_cog("Root")
-    root.embed.add_command(self.create)
 
-@discord.app_commands.command(
-    name="create",
-    description="Create a new embed"
-)
-async def create(
-    self,
-    interaction: discord.Interaction,
-    name: str
-):
+class EmbedCreate(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-    data = {
-        "title": None,
-        "description": None,
-        "color": 0x5865F2,
-        "image": None
-    }
+        root: Root = bot.get_cog("Root")
+        root.embed.add_command(self.create)
 
-    view = EmbedUIView(interaction.guild.id, name, data)
-
-    msg = await interaction.channel.send(
-        embed=view.build_embed(),
-        view=view
+    @discord.app_commands.command(
+        name="create",
+        description="Create a new embed"
     )
+    async def create(
+        self,
+        interaction: discord.Interaction,
+        name: str
+    ):
+        data = {
+            "title": None,
+            "description": None,
+            "color": 0x5865F2,
+            "image": None
+        }
 
-    view.message = msg
+        view = EmbedUIView(
+            guild_id=interaction.guild.id,
+            name=name,
+            data=data
+        )
 
-    await interaction.response.send_message(
-        f"Embed `{name}` editor created.",
-        ephemeral=True
-    )
+        embed = view.build_embed()
+
+        await interaction.response.send_message(
+            embed=embed,
+            view=view
+        )
+
+        message = await interaction.original_response()
+        view.message = message
+
+
+async def setup(bot):
+    await bot.add_cog(EmbedCreate(bot))
