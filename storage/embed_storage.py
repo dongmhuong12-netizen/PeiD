4289cb48f8edd0a421013d872074bbb89c0a447c
dongmhuong12-netizen <dongmhuong12@gmail.com>
2026-03-07 01:32:22 +0700

    Delete storage/embed_storage.py

--- storage/embed_storage.py
+++
@@ -1,9 +0,0 @@
-class EmbedStorage:
-
-    data = {}
-
-    @classmethod
-    def get_guild(cls, guild_id: int):
-        if guild_id not in cls.data:
-            cls.data[guild_id] = {}
-        return cls.data[guild_id]
